a47d7f11bab8154099e3cb3a8d69dadffebfe8c2
csome <clarissasomers@gmail.com>
2018-03-27 17:58:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-DB_USER = 'client24'
-DB_NAME = 'lovematch'
-DB_PASSWORD = 'letmein'
+DB_USER = 'user280'
+DB_NAME = 'ds'
+DB_PASSWORD = 'psswrd'
 DB_HOST = 'localhost'
-
-DB_USER = 'specialist88'
-DB_PASSWORD = 'password'
-
 
 APP_HOST = '127.0.0.1'
 APP_PORT = 5000
